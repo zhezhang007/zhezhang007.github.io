def1c94c0824d650cb3f951fef251517a2bf425d
Jerry <zzhang0712@gmail.com>
2018-03-04 14:58:49 -0800

    Site updated: 2018-03-04 14:58:48

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -28,16 +28,10 @@ $(document).ready(function () {
     }
   }
 
-  // Sidebar float
   function initAffix () {
     var headerHeight = $('.header-inner').height();
     var footerOffset = parseInt($('.main').css('padding-bottom'), 10);
-
-    /*jshint camelcase: false */
-    var sidebarTop = (CONFIG.sidebar.offset_float === 0) ?
-      headerHeight + CONFIG.sidebar.offset :
-      headerHeight;
-    /*jshint camelcase: true */
+    var sidebarTop = headerHeight + 10;
 
     $('.sidebar-inner').affix({
       offset: {
@@ -48,7 +42,7 @@ $(document).ready(function () {
 
     $(document)
       .on('affixed.bs.affix', function () {
-        updateTOCHeight(document.body.clientHeight - 100);
+        updateTOCHeight(document.body.clientHeight - 100)
       });
   }
 
@@ -118,7 +112,6 @@ $(document).ready(function () {
     item.addClass(activeTabClassName);
   });
 
-  // TOC item animation navigate & prevent #item selector in adress bar.
   $('.post-toc a').on('click', function (e) {
     e.preventDefault();
     var targetSelector = NexT.utils.escapeSelector(this.getAttribute('href'));
@@ -135,14 +128,13 @@ $(document).ready(function () {
   });
 
   // Expand sidebar on post detail page by default, when post has a toc.
-  var $tocContent = $('.post-toc-content');
-  var isSidebarCouldDisplay = CONFIG.sidebar.display === 'post' ||
-      CONFIG.sidebar.display === 'always';
-  var hasTOC = $tocContent.length > 0 && $tocContent.html().trim().length > 0;
-  if (isSidebarCouldDisplay && hasTOC) {
-    CONFIG.motion ?
-      (NexT.motion.middleWares.sidebar = function () {
-          NexT.utils.displaySidebar();
-      }) : NexT.utils.displaySidebar();
-  }
+  NexT.motion.middleWares.sidebar = function () {
+    var $tocContent = $('.post-toc-content');
+
+    if (CONFIG.sidebar.display === 'post' || CONFIG.sidebar.display === 'always') {
+      if ($tocContent.length > 0 && $tocContent.html().trim().length > 0) {
+        NexT.utils.displaySidebar();
+      }
+    }
+  };
 });
--- js/src/utils.js
+++ js/src/utils.js
@@ -5,29 +5,24 @@ NexT.utils = NexT.$u = {
    * Wrap images with fancybox support.
    */
   wrapImageWithFancyBox: function () {
-    $('.content img')
-      .not('[hidden]')
-      .not('.group-picture img, .post-gallery img')
-      .each(function () {
-        var $image = $(this);
-        var imageTitle = $image.attr('title');
-        var $imageWrapLink = $image.parent('a');
+    $('.content img').not('.group-picture img').each(function () {
 
-        if ($imageWrapLink.size() < 1) {
-	        var imageLink = ($image.attr('data-original')) ? this.getAttribute('data-original') : this.getAttribute('src');
-          $imageWrapLink = $image.wrap('<a href="' + imageLink + '"></a>').parent('a');
-        }
+      var $image = $(this);
+      var imageTitle = $image.attr('title');
+      var $imageWrapLink = $image.parent('a');
 
-        $imageWrapLink.addClass('fancybox fancybox.image');
-        $imageWrapLink.attr('rel', 'group');
+      if ($imageWrapLink.size() < 1) {
+        $imageWrapLink = $image.wrap('<a href="' + this.getAttribute('src') + '"></a>').parent('a');
+      }
 
-        if (imageTitle) {
-          $imageWrapLink.append('<p class="image-caption">' + imageTitle + '</p>');
+      $imageWrapLink.addClass('fancybox');
+      $imageWrapLink.attr('rel', 'group');
 
-          //make sure img title tag will show correctly in fancybox
-          $imageWrapLink.attr('title', imageTitle);
-        }
-      });
+      if (imageTitle) {
+        $imageWrapLink.append('<p class="image-caption">' + imageTitle + '</p>');
+        $imageWrapLink.attr('title', imageTitle); //make sure img title tag will show correctly in fancybox
+      }
+    });
 
     $('.fancybox').fancybox({
       helpers: {
@@ -40,57 +35,8 @@ NexT.utils = NexT.$u = {
 
   lazyLoadPostsImages: function () {
     $('#posts').find('img').lazyload({
-      //placeholder: '/images/loading.gif',
-      effect: 'fadeIn',
-      threshold : 0
-    });
-  },
-
-  /**
-   * Tabs tag listener (without twitter bootstrap).
-   */
-  registerTabsTag: function () {
-    var tNav = '.tabs ul.nav-tabs ';
-
-    // Binding `nav-tabs` & `tab-content` by real time permalink changing.
-    $(function() {
-      $(window).bind('hashchange', function() {
-        var tHash = location.hash;
-        if (tHash !== '') {
-          $(tNav + 'li:has(a[href="' + tHash + '"])').addClass('active').siblings().removeClass('active');
-          $(tHash).addClass('active').siblings().removeClass('active');
-        }
-      }).trigger('hashchange');
-    });
-
-    $(tNav + '.tab').on('click', function (href) {
-      href.preventDefault();
-      // Prevent selected tab to select again.
-      if(!$(this).hasClass('active')){
-
-        // Add & Remove active class on `nav-tabs` & `tab-content`.
-        $(this).addClass('active').siblings().removeClass('active');
-        var tActive = $(this).find('a').attr('href');
-        $(tActive).addClass('active').siblings().removeClass('active');
-
-        // Clear location hash in browser if #permalink exists.
-        if (location.hash !== '') {
-          history.pushState('', document.title, window.location.pathname + window.location.search);
-        }
-      }
-    });
-
-  },
-
-  registerESCKeyEvent: function () {
-    $(document).on('keyup', function (event) {
-      var shouldDismissSearchPopup = event.which === 27 &&
-        $('.search-popup').is(':visible');
-      if (shouldDismissSearchPopup) {
-        $('.search-popup').hide();
-        $('.search-popup-overlay').remove();
-        $('body').css('overflow', '');
-      }
+      placeholder: '/images/loading.gif',
+      effect: 'fadeIn'
     });
   },
 
@@ -100,15 +46,6 @@ NexT.utils = NexT.$u = {
 
     $(window).on('scroll', function () {
       $top.toggleClass('back-to-top-on', window.pageYOffset > THRESHOLD);
-
-      var scrollTop = $(window).scrollTop();
-      var docHeight = $('#content').height();
-      var winHeight = $(window).height();
-      var contentMath = (docHeight > winHeight) ? (docHeight - winHeight) : ($(document).height() - winHeight);
-      var scrollPercent = (scrollTop) / (contentMath);
-      var scrollPercentRounded = Math.round(scrollPercent*100);
-      var scrollPercentMaxed = (scrollPercentRounded > 100) ? 100 : scrollPercentRounded;
-      $('#scrollpercent>span').html(scrollPercentMaxed);
     });
 
     $top.on('click', function () {
@@ -123,7 +60,9 @@ NexT.utils = NexT.$u = {
   embeddedVideoTransformer: function () {
     var $iframes = $('iframe');
 
-    // Supported Players. Extend this if you need more players.
+    /*
+     * Supported Players. Extend this if you need more players.
+     */
     var SUPPORTED_PLAYERS = [
       'www.youtube.com',
       'player.vimeo.com',
@@ -131,69 +70,56 @@ NexT.utils = NexT.$u = {
       'music.163.com',
       'www.tudou.com'
     ];
-    var pattern = new RegExp( SUPPORTED_PLAYERS.join('|') );
+    var pattern = new RegExp(SUPPORTED_PLAYERS.join('|'));
 
     $iframes.each(function () {
       var iframe = this;
       var $iframe = $(this);
-      var oldDimension = getDimension($iframe);
-      var newDimension;
 
       if (this.src.search(pattern) > 0) {
+        /*
+         * Calculate the video ratio based on the iframe's w/h dimensions
+         */
+        var videoRatio = ( $iframe.height() / $iframe.width() ) * 100;
 
-        // Calculate the video ratio based on the iframe's w/h dimensions
-        var videoRatio = getAspectRadio(oldDimension.width, oldDimension.height);
+        // Add height for 163 music.
+        if (this.src.search('music.163.com') > 0) {
+          videoRatio += 10;
+        }
 
-        // Replace the iframe's dimensions and position the iframe absolute
-        // This is the trick to emulate the video ratio
-        $iframe.width('100%').height('100%')
+        /*
+         * Replace the iframe's dimensions and position
+         * the iframe absolute, this is the trick to emulate
+         * the video ratio
+         */
+        $iframe
+          .width('100%')
+          .height('100%')
           .css({
             position: 'absolute',
             top: '0',
             left: '0'
           });
 
-
-        // Wrap the iframe in a new <div> which uses a dynamically fetched padding-top property
-        // based on the video's w/h dimensions
+        /*
+         * Wrap the iframe in a new <div> which uses a
+         * dynamically fetched padding-top property based
+         * on the video's w/h dimensions
+         */
         var wrap = document.createElement('div');
         wrap.className = 'fluid-vids';
-        wrap.style.position = 'relative';
-        wrap.style.marginBottom = '20px';
         wrap.style.width = '100%';
+        wrap.style.position = 'relative';
         wrap.style.paddingTop = videoRatio + '%';
-        // Fix for appear inside tabs tag.
-        (wrap.style.paddingTop === '') && (wrap.style.paddingTop = '50%');
 
-        // Add the iframe inside our newly created <div>
+        /*
+         * Add the iframe inside our newly created <div>
+         */
         var iframeParent = iframe.parentNode;
         iframeParent.insertBefore(wrap, iframe);
         wrap.appendChild(iframe);
-
-        // Additional adjustments for 163 Music
-        if (this.src.search('music.163.com') > 0) {
-          newDimension = getDimension($iframe);
-          var shouldRecalculateAspect = newDimension.width > oldDimension.width ||
-                                        newDimension.height < oldDimension.height;
-
-          // 163 Music Player has a fixed height, so we need to reset the aspect radio
-          if (shouldRecalculateAspect) {
-            wrap.style.paddingTop = getAspectRadio(newDimension.width, oldDimension.height) + '%';
-          }
-        }
       }
     });
-
-    function getDimension($element) {
-      return {
-        width: $element.width(),
-        height: $element.height()
-      };
-    }
-
-    function getAspectRadio(width, height) {
-      return height / width * 100;
-    }
   },
 
   /**
@@ -203,7 +129,7 @@ NexT.utils = NexT.$u = {
   addActiveClassToMenuItem: function () {
     var path = window.location.pathname;
     path = path === '/' ? path : path.substring(0, path.length - 1);
-    $('.menu-item a[href^="' + path + '"]:first').parent().addClass('menu-item-active');
+    $('.menu-item a[href="' + path + '"]').parent().addClass('menu-item-active');
   },
 
   hasMobileUA: function () {
@@ -237,7 +163,7 @@ NexT.utils = NexT.$u = {
   },
 
   displaySidebar: function () {
-    if (!this.isDesktop() || this.isPisces() || this.isGemini()) {
+    if (!this.isDesktop() || this.isPisces()) {
       return;
     }
     $('.sidebar-toggle').trigger('click');
@@ -249,10 +175,6 @@ NexT.utils = NexT.$u = {
 
   isPisces: function () {
     return CONFIG.scheme === 'Pisces';
-  },
-
-  isGemini: function () {
-    return CONFIG.scheme === 'Gemini';
   },
 
   getScrollbarWidth: function () {
@@ -271,6 +193,6 @@ NexT.utils = NexT.$u = {
    * @returns {Boolean}
    */
   needAffix: function () {
-    return this.isPisces() || this.isGemini();
+    return this.isPisces();
   }
 };
